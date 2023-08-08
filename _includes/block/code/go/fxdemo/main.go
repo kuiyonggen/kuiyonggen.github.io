1d577911b0e1df01a076fe405f7542dd6a0f103e
alice <alice@apple>
2023-08-08 12:55:35 +0800

    update post

--- _includes/block/code/go/fxdemo/main.go
+++ _includes/block/code/go/fxdemo/main.go
@@ -1,41 +1,63 @@
 package main
 
 import (
-    "fmt"
     "io"
-    "os"
+    "io/ioutil"
     "context"
     "net"
     "net/http"
+    "go.uber.org/zap"
     "go.uber.org/fx"
+    "go.uber.org/fx/fxevent"
 )
 
 
+// Route is an http.Handler that knows the mux pattern
+// under which it will be registerd.
+type Route interface {
+    http.Handler
+
+    // Pattern reports the path at which this is registered.
+    Pattern() string
+}
+
 // EchoHandler is an http.Handler that copies its request body back to the response.
-type EchoHandler struct{}
+type EchoHandler struct{
+    log *zap.Logger
+}
+
+func (*EchoHandler) Pattern() string {
+    return "/echo"
+}
 
 // NewEchoHandler builds a new EchoHandler.
-func NewEchoHandler() *EchoHandler {
-    return &EchoHandler{}
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+    return &EchoHandler{log: log}
 }
 
 // ServerHttp handles an HTTP request to the /echo endpoint.
-func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        h.log.Warn("Error reading request body")
+        return
+    }
+    h.log.Info("Received a request", zap.String("body", string(body[:])))
     if _, err := io.Copy(w, r.Body); err != nil {
-        fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+        h.log.Warn("Failed to handle request", zap.Error(err))
     }
 }
 
 // NewServeMux builds a ServeMux that will route requests to the given EchoHandler
-func NewServeMux(echo *EchoHandler) *http.ServeMux {
+func NewServeMux(route Route) *http.ServeMux {
     mux := http.NewServeMux()
-    mux.Handle("/echo", echo)
+    mux.Handle(route.Pattern(), route)
     return mux
 }
 
 // NewHTTPServer builds an HTTP server that will begin serving requests
 // when the Fx application starts.
-func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
     srv := &http.Server{Addr: ":8884", Handler: mux}
     lc.Append(fx.Hook{
         OnStart: func(ctx context.Context) error {
@@ -43,12 +65,13 @@ func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
             if err != nil {
                 return err
             }
-            fmt.Println("Starting HTTP server at", srv.Addr)
+            log.Info("Starting HTTP server at", zap.String("addr", srv.Addr))
             go srv.Serve(ln)
             return nil
         },
 
         OnStop: func(ctx context.Context) error {
+            log.Info("Stopping HTTP server.")
             return srv.Shutdown(ctx)
         },
     })
@@ -59,10 +82,17 @@ func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
 
 func main() {
     fx.New(
+        fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+            return &fxevent.ZapLogger{Logger: log}
+        }),
         fx.Provide(
             NewHTTPServer,
             NewServeMux,
-            NewEchoHandler,
+            fx.Annotate(
+                NewEchoHandler,
+                fx.As(new(Route)),
+             ),
+            zap.NewExample,
         ),
         fx.Invoke(func(*http.Server) {}),
     ).Run()
---
+++ _posts/2023-08-08-a-story-of-a-hundred-and-fifty-years-ago.md
@@ -0,0 +1,31 @@
+---
+layout: post
+title: "A Story Of A Hundred And Fifty Years Ago"
+categories: ["story", "kids"]
+mermaid: true
+---
+There is a man who is living in a mordern city. He was curious about his family when he once was a kid.
+
+He heared some words from talks of the big families that his grand grand grand father transferred to the present hometown from some north place. The kid was very upset for the words. He had a natural conciousness that the hometown was not his root.
+
+He had no grand father when he was born. He was upset about it in his childhood.So the truth of his unkonwn root was another sad thing for him. Because he thought it is like a tree loosing its root. Where is his root? He always questioned himself. Can it be found in any way?
+
+One day, he visited his parent. His father told him about the root which was from one of  grandfathers of the man that the grand grand father of his father might be taken by some person from some place of Eastern Moutain district around the age of ten. The man realised quickly that the taken boy had been kidnapped to their new hometown and was raised by the new family.
+
+The man ask the detail ages about the related persons to his father, estimated the birthdays of the first two generations and the kidnapping year, and then draw a flowchart of the ages, showing in the following.
+
+<div class="mermaid">
+flowchart LR
+one(("1 1867(1877)")) --> two((2 1897)) --> three((3 1927/1929)) --> four((4 1954)) --> five((5 1984)) --> six((6 2013))
+
+style one fill:#a1a,color:red
+style two fill:#b2b,color:pink
+style three fill:#c3c,color:yellow
+style four fill:#d4d,color:yellow
+style five fill:#e5e,color:yellow
+style six fill:#f6f,color:yellow
+</div>
+
+So the unlucky boy was esimated to be kidnapping in 1877. How could the man do anything to find his root? There is no any other evidences. He thought of the DNA match, but what the huge cost. There is no way for the man for the present. He  has no choice and must continue to looking for new ones.
+
+What's the denstiny of the man? Can he find his original root in his life? I don't know. But I'm also sad for him about the story of a hundred and fifty years ago.
