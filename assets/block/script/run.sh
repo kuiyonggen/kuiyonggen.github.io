a52d0f8377a748e2bfecbd83c930593479651d4c
kyg <dev@kyg>
2024-04-12 11:23:38 +0800

    add ads

--- assets/block/script/run.sh
+++ assets/block/script/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# 添加镜像源并移除默认源
+gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove https://rubygems.org/
+# 列出已有源
+gem sources -l
+# 应该只有镜像源一个
+bundle config mirror.https://rubygems.org https://mirrors.tuna.tsinghua.edu.cn/rubygems
+gem install bundler jekyll
+
 REPO_DIR=$(dirname $(dirname $(dirname $(dirname $(realpath  "$0")))))
 cd ${REPO_DIR}
 cp -rf _includes/block assets/
