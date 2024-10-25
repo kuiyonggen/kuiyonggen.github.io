d80b2046a66bdf2c7d0266267c6efce3b144769f
kyg <dev@kuiyonggen.com>
2024-10-26 00:08:48 +0800

    new post

--- assets/block/script/run.sh
+++ assets/block/script/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LOCAL=$1
+cp -rf _includes/block assets/
 if [ ! $LOCAL ];then
     # 添加镜像源并移除默认源
     gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove https://rubygems.org/
@@ -11,10 +12,12 @@ if [ ! $LOCAL ];then
     bundle config mirror.https://rubygems.org https://mirrors.tuna.tsinghua.edu.cn/rubygems
     REPO_DIR=$(dirname $(dirname $(dirname $(dirname $(realpath  "$0")))))
     cd ${REPO_DIR}
-    cp -rf _includes/block assets/
     bundle install
     cd -
 else
     echo 'Run local.'
 fi
-bundle exec jekyll serve --livereload --open-url --host 127.0.0.1 --port 8084
+
+#bundle exec jekyll serve --config _config.yml --livereload --open-url --host 127.0.0.1 --port 8084
+bundle exec jekyll serve --config ._config.yml --livereload --open-url --host 127.0.0.1 --port 8084
+#bundle exec jekyll serve --livereload --open-url --host 0.0.0.0 --port 8084
